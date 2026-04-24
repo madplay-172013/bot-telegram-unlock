fb7046a026b143f68ea36d2e0b8776a0e379ecbc
madplay-172013 <madplay.iptv@gmail.com>
2026-04-24 14:08:35 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 pip install -r requirements.txt
-playwright install chromium
-playwright install-deps chromium
+python -m playwright install chromium
+python -m playwright install-deps chromium
